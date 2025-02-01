7f0f603e0822ae384f7b153b210bf7bfd8f4431a
ahmobaby <ahmed.atif.uzun@gmail.com>
2025-02-01 20:46:55 +0300

    Added Player movement
    -Added player spritesheet
    -Moved the player

--- rpg game/main.cpp
+++ rpg game/main.cpp
@@ -4,15 +4,19 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "RPG GAME");
-	sf::CircleShape shape(50);
-	sf::RectangleShape shape2(sf::Vector2f(100, 200));
-	shape.setPosition(sf::Vector2f(200, 300));
-	sf::RectangleShape shape3(sf::Vector2f(150, 5));
-	shape2.setPosition(500, 200);
-    shape.setOutlineThickness(10);
-	shape3.setRotation(60);
-	shape3.setPosition(300, 100);
 	
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+     
+	playerTexture.loadFromFile("Assets/Player/Textures/spritesheet.png");
+	playerSprite.setTexture(playerTexture);
+	int XIndex = 0;
+	int YIndex = 0;
+	playerSprite.setTextureRect(sf::IntRect(XIndex * 64, YIndex * 64, 64, 64));
+    playerSprite.scale(2, 2);
+	playerSprite.setPosition(100, 200);
+
+  
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -22,15 +26,25 @@ int main()
 
 			if (event.type == event.Closed)
 				window.close();
-		}
+         }
+          
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		
+			playerSprite.move(0.5, 0);
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		playerSprite.move(-0.5, 0);
+		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		playerSprite.move(0, -0.5);
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+	    playerSprite.move(0, 0.5);
+		
+
+
 		window.clear(sf::Color::Black);
-		window.draw(shape2);
-		window.draw(shape3);
-		window.draw(shape);
-		shape.setFillColor(sf::Color::Red);
-		shape2.setFillColor(sf::Color::Blue);
-		shape.setOutlineColor(sf::Color::Yellow);
-		shape3.setFillColor(sf::Color::Green);
+		window.draw(playerSprite);
 		window.display();
 	}
 
