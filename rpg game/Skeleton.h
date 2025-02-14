29a075f2d6b6a7268112d473d47b4173d19b9667
ahmobaby <ahmed.atif.uzun@gmail.com>
2025-02-14 22:59:42 +0300

    deltatime + AABB
    -Limited fps
    -Added deltatime clock
    -Added AABB
    -Rendered debug AABB for skeleton and player

--- rpg game/Skeleton.h
+++ rpg game/Skeleton.h
@@ -1,18 +1,24 @@
-
 #pragma once
 #include <SFML/Graphics.hpp>
-
 class Skeleton
 {
 private:
 	sf::Texture texture;
+	std::vector<sf::RectangleShape>bullets;
+	sf::RectangleShape boundingRectangle;
+	sf::Vector2i size;
+	float skeletonSpeed = 0.5;
 
 public:
 	sf::Sprite sprite;
-
 public:
 	void Initialize();
 	void Load();
-	void Update();
+	void Update(float deltaTime);
 	void Draw(sf::RenderWindow& window);
+
+
+
+
+
 };
